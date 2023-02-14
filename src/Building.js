c5ddcba7e3cce79b5f1f744929e6ea454ca39103
Creepler13 <denis.schlichting03@gmail.com>
2023-02-14 14:56:59 +0100

    starting with tiles

--- src/Building.js
+++ src/Building.js
@@ -2,22 +2,53 @@ const Tile = require("./Tile");
 
 module.exports = class Building extends Tile {
     constructor(x, y, facing, id) {
-        super(x, y, "building");
+        super(x, y, Tile.types.building, id);
         this.facing = facing;
         this.level = 1;
         this.space = [];
         this.spaceType = [];
-        this.canUpdate = false;
         this.currentUpdateTick = 0;
     }
 
-
-    upgrade(){
-        
+    upgrade() {
         this.level++;
-    };
+    }
 
-    update(){
+    update() {
+        let data = this.getData();
 
+        if (!data.action) return;
+        this.currentUpdateTick++;
+        if (this.currentUpdateTick < data.updateTicks) return;
+
+
+
+        this.currentUpdateTick=0; 
+    }
+
+    /**
+     *
+     * @returns {BuildingData}
+     */
+    getData() {
+        return super.getData();
     }
 };
+
+/**
+ * @typedef{{
+ *      name:string
+ *      texture:HTMLImageElement
+ *      updateTicks:?number
+ *      cost:{}
+ *      upgrade:?[{
+ *                 cost:number
+ *                 mod:?{}
+ *                 texture:?HTMLImageElement
+ *               }]
+ *      action:?[{
+ *              type:string
+ *              data:?{}
+ *              }]
+ * }} BuildingData
+ */
--- src/Tile.js
+++ src/Tile.js
@@ -1,11 +1,21 @@
-class Tile {
-    constructor(x, y, type) {
+module.exports =class Tile {
+    constructor(x, y, type, id) {
         this.x = x;
         this.y = y;
         this.type = type;
+        this.id;
     }
 
-    getData()
+    static types = {
+        building: "buildings",
+        material: "materials",
+    };
+
+
+
+   
+    getData() {
+        return window.gameData[this.type][this.id];
+    }
 }
 
-module.exports = Tile;
